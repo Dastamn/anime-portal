2282ee64adb28de225475c885156b8cbe2d777c4
Dastamn <mezhoudi.nazim@gmail.com>
2022-12-13 15:43:26 +0100

    Set body overflow-x to hidden

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import MediaList from "./components/media/MediaList";
 
 export default function Home() {
   return (
-    <div>
+    <>
       {/* @ts-expect-error Server Component */}
       <MediaList title="Test" link="/" />
-    </div>
+    </>
   );
 }
